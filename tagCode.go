58835f5c84013acfbd6d90eb7c5d60087eea79c0
dekopon21020014 <renueta@icloud.com>
2024-05-04 13:57:15 +0900

    すべてのタグコードを定数にした

--- tagCode.go
+++ tagCode.go
@@ -20,8 +20,36 @@ const (
 	LDN               = 0x09
 	INFORMATION       = 0x15
 	FILTER            = 0x11
-	IDP = 0x0f
-	DATA = 0x1e
+	IDP               = 0x0f
+	DATA              = 0x1e
 
-	PREAMBLE = 0x40
+	// for Mfer.Control
+	BYTE_ORDER   = 0x01
+	VERSION      = 0x02
+	CHAR_CODE    = 0x03
+	ZERO         = 0x00
+	COMMENT      = 0x16
+	MACHINE_INFO = 0x17
+	COMPRESSION  = 0x0e
+
+	// for extensions
+	PREAMBLE  = 0x40
+	EVENT     = 0x41
+	VALUE     = 0x42
+	CONDITION = 0x44
+	ERROR     = 0x43
+	GROUP     = 0x67
+	POINTER   = 0x45
+	SIGNITURE = 0x46
+	
+	// for Mfer.Helper
+	P_NAME = 0x81
+	P_ID  = 0x82
+	P_AGE = 0x83
+	P_SEX = 0x83
+	TIME = 0x85
+	MESSAGE = 0x86
+	UID = 0x87
+	MAP = 0x88
+	END = 0x80
 )
