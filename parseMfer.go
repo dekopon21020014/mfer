b8d39a03b657ab8ca836fb0d7075b919aab5e5bf
dekopon21020014 <renueta@icloud.com>
2024-05-04 14:06:26 +0900

    わかるところは記述．フォーマットが理解できていない部分は未実装

--- parseMfer.go
+++ parseMfer.go
@@ -18,15 +18,14 @@ func parseMfer(bytes []byte) Mfer {
 		i++
 		length = bytes[i]
 		i++
-		// fmt.Printf("tagCode = %02x, length = %02x(%d), i = %d\n", tagCode, length, length, i)
+		fmt.Printf("tagCode = %02x, length = %02x(%d), i = %d\n", tagCode, length, length, i)
 
 		switch tagCode { /* tag名を出力する */
 		// for Mfer.Sampling
 		case INTERVAL: /* サンプリング間隔とか */
 			mfer.Sampling.Interval.UnitCode = bytes[i]
 			mfer.Sampling.Interval.Exponent = int8(bytes[i+1])
-			binaryMantissa := bytes[i+2 : i+int(length)]
-			mfer.Sampling.Interval.Mantissa = Binary2Uint32(mfer.Control.ByteOrder, binaryMantissa...)
+			mfer.Sampling.Interval.Mantissa = Binary2Uint32(mfer.Control.ByteOrder, bytes[i+2:i+int(length)]...)
 
 		case SENSITIVITY: /* サンプリング解像度 */
 			// 得られるのはコードだから文字列に変換したいなら別途処理が必要
@@ -56,7 +55,7 @@ func parseMfer(bytes []byte) Mfer {
 			binaryNumSequence := bytes[i : i+int(length)]
 			mfer.Frame.NumSequence = Binary2Uint32(mfer.Control.ByteOrder, binaryNumSequence...)
 
-		case POINTER:
+		case F_POINTER:
 			/* do somthing */
 
 		// for Mfer.WaveFrom
@@ -73,7 +72,7 @@ func parseMfer(bytes []byte) Mfer {
 				Data:    (bytes[i+2 : (i + 2 + int(bytes[i+1]))]),
 			}
 			mfer.Frame.Channels = append(mfer.Frame.Channels, channnel)
-		
+
 		// for mfer.WaveForm
 		case LDN:
 			// 2byte 以上のこともある？
@@ -90,28 +89,89 @@ func parseMfer(bytes []byte) Mfer {
 			// 2byte以上のこともあり？
 			mfer.WaveForm.IpdCode = bytes[i]
 
-		case 0x17: /* 波形データの制作者 */
-			mfer.Control.MchineInfo = string(bytes[i : i+int(length)])
-
-		case 0x01: /* バイトオーダー */
-			mfer.Control.ByteOrder = bytes[i]
-
 		case DATA: /* 波形データ部 */
-			fmt.Printf("length = %x\n", length)
 			// tagやデータ長は無条件でビッグエンディアん
-			dataLength := Binary2Uint32(0, bytes[i : i+4]...)
-			fmt.Printf("DataLength = %d\n", dataLength)
-			i = i+4
+			// dataLength := Binary2Uint32(0, bytes[i : i+4]...)
+			i = i + 4
 			// mfer.WaveForm.Data = bytes[i : i + int(dataLength)] // この行は必要だけど見にくく成るのでコメントアウトしておく
 
+		// for Mfer.Control
+		case BYTE_ORDER:
+			mfer.Control.ByteOrder = bytes[i]
+
+		case VERSION:
+			// なんだこれ？
+			mfer.Control.Version = bytes[i : i+3]
+
+		case CHAR_CODE:
+			mfer.Control.CharCode = string(bytes[i : i+int(length)])
+
+		case ZERO:
+			i--
+
+		case COMMENT:
+			mfer.Control.Comment += string(bytes[i : i+int(length)])
+
+		case MACHINE_INFO:
+			mfer.Control.MachineInfo = string(bytes[i : i+int(length)])
+
+		case COMPRESSION:
+			fmt.Printf("COMPRESSTION\n")
+			// ここもいまいちよくわからない
+			i--
+			mfer.Control.CompressionCode = binary.BigEndian.Uint16(bytes[i : i+2])
+			// length = binary.BigEndian.Uint32(bytes[i+2 : i+6])
+
+		// for Mfer.Extensions
 		case PREAMBLE: /* プリアンブル */
 			mfer.Extensions.Preamble = string(bytes[i : i+int(length)])
-		}
 
-		if tagCode == DATA { /* 波形データ部まできたらループ終了 */
+		case EVENT:
+			/* よくわかっていない */
+			i--
+			mfer.Extensions.Event.Code = bytes[i]
+			// ここはビッグエンディアンなのかリトルエンディアンなのか...
+			mfer.Extensions.Event.Begin = binary.BigEndian.Uint32(bytes[i+2 : i+6])
+			mfer.Extensions.Event.Duration = binary.BigEndian.Uint32(bytes[i+6 : i+10])
+			mfer.Extensions.Event.Info = string(bytes[i+10 : i+10+256])
+
+		// この辺のフォーマットがよくわかっていない
+		case VALUE:
+		case CONDITION:
+		case ERROR:
+		case GROUP:
+		case R_POINTER:
+		case SIGNITURE:
+
+		// for Mfer.Helper
+		case P_NAME:
+			mfer.Helper.Patient.Name = string(bytes[i : i+int(length)])
+
+		case P_ID:
+			mfer.Helper.Patient.Id = string(bytes[i : i+int(length)])
+
+		case P_AGE:
+			mfer.Helper.Patient.Age = bytes[i]
+			mfer.Helper.Patient.AgeInDays = Binary2Uint32(mfer.Control.ByteOrder, bytes[i+1 : i+3]...)
+			mfer.Helper.Patient.BirthYear = Binary2Uint32(mfer.Control.ByteOrder, bytes[i+3 : i+5]...)
+			mfer.Helper.Patient.BirthMonth = bytes[i+5]
+			mfer.Helper.Patient.BirthDay = bytes[i+6]
+
+		case P_SEX:
+			mfer.Helper.Patient.Sex = bytes[i]
+
+		case TIME:
+			// よくわかっていない
+
+		case MESSAGE:
+		case UID:
+
+		case MAP:
+		case END:
+			fmt.Printf("END\n")
 			break
 		}
-
+		
 		i += int(length)
 	}
 	return mfer
